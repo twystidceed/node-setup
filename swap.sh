9c702ff0f0a9bcdfb7921a5966612471caf52948
Twystidceed <kjwacikowski@gmail.com>
2018-09-24 04:07:19 +0200

    Add files via upload

---
+++ swap.sh
@@ -0,0 +1,16 @@
+if grep -q "SwapTotal" /proc/meminfo; then
+    echo -e "${GREEN}Skipping disk swap configuration...${NC} \n"
+else
+    echo -e "${YELLOW}Creating 2GB disk swap file. \nThis may take a few minutes!${NC} \a"
+    touch /var/swap.img
+    chmod 600 swap.img
+    dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+    mkswap /var/swap.img 2> /dev/null
+    swapon /var/swap.img 2> /dev/null
+    if [ $? -eq 0 ]; then
+        echo '/var/swap.img none swap sw 0 0' >> /etc/fstab
+        echo -e "${GREEN}Swap was created successfully!${NC} \n"
+    else
+        echo -e "${RED}Operation not permitted! Optional swap was not created.${NC} \a"
+        rm /var/swap.img
+    fi
