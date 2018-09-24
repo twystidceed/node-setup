9a5f3d61ebfb30e39e647d60b49faa467eb85d0f
Twystidceed <kjwacikowski@gmail.com>
2018-09-24 04:13:42 +0200

    Add files via upload

--- node-setup.sh
+++ node-setup.sh
@@ -58,4 +58,22 @@ sudo apt-get -y install make libtool autoconf libboost-dev  libgmp3-dev ufw pkg-
 
 
 #Create 2GB swap file
-swap.sh
+if grep -q "SwapTotal" /proc/meminfo; then
+    echo -e "${GREEN}Skipping disk swap configuration...${NC} \n"
+else
+    echo -e "${YELLOW}Creating 2GB disk swap file. \nThis may take a few minutes!${NC} \a"
+    touch /var/swap.img
+    chmod 600 swap.img
+    dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+    mkswap /var/swap.img 2> /dev/null
+    swapon /var/swap.img 2> /dev/null
+    if [ $? -eq 0 ]; then
+        echo '/var/swap.img none swap sw 0 0' >> /etc/fstab
+        echo -e "${GREEN}Swap was created successfully!${NC} \n"
+    else
+        echo -e "${RED}Operation not permitted! Optional swap was not created.${NC} \a"
+        rm /var/swap.img
+    fi
+fi
+
+# EOF
